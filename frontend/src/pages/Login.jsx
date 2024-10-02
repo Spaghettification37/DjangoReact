b3a57a44d3fe6d043f6988b560888f8b0ef61c5c
Spaghettification37 <stewartericemail@gmail.com>
2024-10-02 12:07:29 -0700

    test push

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,6 @@
 import LoginForm from "../components/LoginForm"
 
 function Login() {  //This will call the Form created in Form.jsx using the following props (properties)
-    console.log("Going to login")
     return <LoginForm route="/api/token/" />
 }
 
